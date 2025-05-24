78bcb0c106e8e4aa416792d228c5a363239ad065
Tu Phan <pt27122000@gmail.com>
2025-05-25 00:00:47 +0700

    cofig: add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-svh">
+      <div className='flex flex-col items-center justify-center min-h-svh'>
         <Button>Click me</Button>
       </div>
     </>
