891cadce25ec9214467bc3d0facfdb85cc0bbce1
Tu Phan <pt27122000@gmail.com>
2025-05-25 14:26:39 +0700

    chore: add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { Button } from '@/components/ui/button';
+import { Button } from './components/ui/button';
+import { Input } from './components/ui/input';
+import { ThemeProvider } from './components/theme-provider';
 
 function App() {
   return (
-    <>
+    <ThemeProvider>
       <div className='flex flex-col items-center justify-center min-h-svh'>
         <Button>Click me</Button>
+        <Input placeholder='Type something...' className='mt-4' />
       </div>
-    </>
+    </ThemeProvider>
   );
 }
 
