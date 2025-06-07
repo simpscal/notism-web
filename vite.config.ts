4b4a3238de7ca9e9428859215c23f51f3f2601ce
Tu Phan <pt27122000@gmail.com>
2025-06-07 15:12:22 +0700

    fix: add envPrefix to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import path from 'path';
+
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  envPrefix: ['VITE_', 'REACT_APP_'],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
