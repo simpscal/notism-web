19175a173ea9e8b5b19049198e6406a15aaa731f
simpscal <pt27122000@gmail.com>
2025-11-02 20:51:27 +0700

    fix: update path aliases to use '@/' prefix in tsconfig and vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,12 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@utils': path.resolve(__dirname, './src/shared/utils'),
-      '@models': path.resolve(__dirname, './src/shared/models'),
-      '@enums': path.resolve(__dirname, './src/shared/enums'),
-      '@constants': path.resolve(__dirname, './src/shared/constants'),
-      '@hooks': path.resolve(__dirname, './src/core/hooks'),
-      '@providers': path.resolve(__dirname, './src/core/providers'),
+      '@/utils': path.resolve(__dirname, './src/shared/utils'),
+      '@/models': path.resolve(__dirname, './src/shared/models'),
+      '@/enums': path.resolve(__dirname, './src/shared/enums'),
+      '@/constants': path.resolve(__dirname, './src/shared/constants'),
+      '@/hooks': path.resolve(__dirname, './src/core/hooks'),
+      '@/providers': path.resolve(__dirname, './src/core/providers'),
     },
   },
 });
