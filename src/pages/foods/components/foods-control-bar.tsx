ebe11a020e3af8feb0f3ba78845773e0be5d909d
Tu Phan <58072105+simpscal@users.noreply.github.com>
2026-06-07 16:40:43 +0700

    fix(#197): remove sticky positioning from categories filter on mobile (#55)
    * fix(#197): remove sticky positioning from categories filter on mobile
    
    * fix(#197): remove sticky positioning from categories filter on all screens

--- src/pages/foods/components/foods-control-bar.tsx
+++ src/pages/foods/components/foods-control-bar.tsx
@@ -39,7 +39,7 @@ function FoodsControlBar({
     };
 
     return (
-        <div className='sticky top-16 z-40 -mx-4 mb-5 border-b bg-background/95 px-4 pb-3 pt-1 backdrop-blur supports-[backdrop-filter]:bg-background/80 sm:mb-6'>
+        <div className='-mx-4 mb-5 border-b bg-background/95 px-4 pb-3 pt-1 backdrop-blur supports-[backdrop-filter]:bg-background/80 sm:mb-6'>
             <div className='flex items-center gap-3'>
                 <div className='min-w-0 flex-1 overflow-x-auto pb-1 [scrollbar-width:none] [&::-webkit-scrollbar]:hidden'>
                     <ToggleGroup
